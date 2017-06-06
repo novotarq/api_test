af043f66c960101e94e6174ee716c7dc33b5f5d9
Michal Nowotarski <novotarq@gmail.com>
2017-06-06 17:20:33 +0200

    change test type

--- test/services/page_service_test.rb
+++ test/services/page_service_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PageServiceTest < ActiveJob::TestCase
+class PageServiceTest < ActiveSupport::TestCase
 
   def setup
     @mock_file = Nokogiri::HTML::Document.parse('<html><h1>a</h1><h2>b</h2><a href="http://test.com">Test</a><a href="http://test2.com">Test</a></html>')
