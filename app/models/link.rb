09fa59909056df424622d4d44f7e7ebf1cc8d121
Michal Nowotarski <novotarq@gmail.com>
2017-06-06 15:04:02 +0200

    link validations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link < ApplicationRecord
   belongs_to :page
 
-  validates_presence_of :url
   validates_presence_of :page
+  validates :url, presence: true, format: { with: URI::regexp(%w(http https)) }
 end
--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -2,5 +2,17 @@ require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
   should validate_presence_of :page
-  should validate_presence_of :url
+
+  def setup
+    @page = Page.create!(url: 'http://google.com/')
+  end
+
+  test 'validations' do
+    assert Link.new(url: '', page: @page).invalid?
+    assert Link.new(url: nil, page: @page).invalid?
+    assert Link.new(url: 'test', page: @page).invalid?
+    assert Link.new(url: 'https://test.com/', page: nil).invalid?
+    assert Link.new(url: 'http://test.com/', page: @page).valid?
+    assert Link.new(url: 'https://test.com/', page: @page).valid?
+  end
 end
