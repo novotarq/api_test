002baecb1292031c72ee0a71b47b58505ab5082b
Michal Nowotarski <novotarq@gmail.com>
2017-06-06 14:18:06 +0200

    test enqueuing the job

--- test/jobs/page_job_test.rb
+++ test/jobs/page_job_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class PageJobTest < ActiveJob::TestCase
-  test 'nothing for now' do
+  test 'enqueuning' do
     assert_enqueued_with(job: PageJob) do
-      PageJob.perform_later(Page.new('http://test.com'))
+      PageJob.perform_later(Page.create!(url: 'http://test.com'))
     end
   end
 end
